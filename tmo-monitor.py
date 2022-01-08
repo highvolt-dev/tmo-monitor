0a2a47c001f03a791669f2543b09f18e301b4094
Andrew Delikat <andrew@delikat.moe>
2022-01-08 04:19:31 -0500

    Support macOS ping output format

--- tmo-monitor.py
+++ tmo-monitor.py
@@ -17,6 +17,7 @@ import re
 import tailer
 from parse import *
 
+
 def print_and_log(msg, level='INFO'):
   print(msg)
   lvl = logging.getLevelName(level)
@@ -147,7 +148,7 @@ class TrashCanController:
         return -1
       if is_win and 'Destination host unreachable' in str(ping_exec.stdout):
         return -1
-      pattern = b'[rtt|round-trip] min/avg/max/mdev = \d+.\d+/(\d+.\d+)/\d+.\d+/\d+.\d+ ms'
+      pattern = b'[rtt|round-trip] min/avg/max/(?:mdev|stddev) = \d+.\d+/(\d+.\d+)/\d+.\d+/\d+.\d+ ms'
       if is_win:
         pattern = b'Minimum = \d+ms, Maximum = \d+ms, Average = (\d+)ms'
       ping_ms = re.search(pattern, ping_exec.stdout)
