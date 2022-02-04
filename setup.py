280b4b75191d87df09b6021fa03278fdbbeaac0d
highvolt-dev <highvoltage@gmail.com>
2022-02-03 21:39:14 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='tmo_monitor',
-  version='2.0.0-dev',
+  version='2.0.0-beta4',
   description='A script to monitor the T-Mobile Home Internet 5G Gateways',
   long_description='A lightweight, cross-platform Python 3 script that can monitor the T-Mobile Home Internet Arcadyan and Nokia 5G Gateways for 4G/5G bands, cellular site (tower), and internet connectivity and reboots as needed or on-demand.',
   url='https://github.com/highvolt-dev/tmo-monitor',
